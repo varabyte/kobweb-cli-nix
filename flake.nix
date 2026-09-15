4ef206c35000aaeba16c03399bcdfcb513e048ba
David Herman <bitspittle@gmail.com>
2026-09-15 09:51:51 -0700

    Remove legacy x86_64-darwin from supported systems
    Nixpkgs is dropping it, so we should too to prevent
    potential evaluation errors for a majority of nix
    users.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: let
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
     overlays.default = import ./overlay.nix;
